ad2750d25dad6b325fb1c0cfc32e65b2caa558ae
sslyxhz <sslyxhz@hotmail.com>
2018-11-01 01:52:19 +0800

    rename modules

--- lib_base_module/src/main/java/com/sslyxhz/base/AppConfig.java
+++ lib_base_module/src/main/java/com/sslyxhz/base/AppConfig.java
@@ -1,7 +1,7 @@
 package com.sslyxhz.base;
 
 public class AppConfig {
-    private static final String LoginApp = "com.loong.login.LoginApp";
+    private static final String LoginApp = "com.sslyxhz.login.LoginApp";
 
     public static String[] moduleApps = {
             LoginApp
